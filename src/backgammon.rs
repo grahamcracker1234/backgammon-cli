d9a9ba510fec62a7689b7158a00538ac75717cb4
Graham Preston <grahampreston@comcast.net>
2023-01-03 04:14:37 -0500

    update backgammon.rs

--- src/backgammon.rs
+++ src/backgammon.rs
@@ -26,9 +26,8 @@ pub(crate) enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::InvalidNotationPosition(pos) | Error::InvalidIndexPosition(pos) => {
-                write!(f, "position '{pos}' is not valid")
-            }
+            Error::InvalidNotationPosition(pos) => write!(f, "position '{pos}' is not valid"),
+            Error::InvalidIndexPosition(index) => write!(f, "index '{index}' is not valid"),
             Error::InvalidNotation(notation) => write!(f, "notation '{notation}' is not valid"),
             Error::InvalidPlayLength(len) => write!(f, "play of length '{len}' is not valid"),
             Error::IncompleteTurn => write!(f, "did not use all possible plays"),
